031cfb8e2f43db063b49adb875ec17a602cc50df
rabiulhossen <rhsimon69@gmail.com>
2022-06-01 01:28:58 +0600

    header-sectin completed

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import './Intro.css'
+import facebook from '../../../src/images/529-removebg-preview.png'
+import github from '../../../src/images/github.png'
+import rabiul from '../../../src/images/instagrajm.png'
+import insta  from '../../../src/images/instagram.png'
+import bg  from '../../../src/images/abtract-background-3.jpg'
 
 export default function Intro() {
   return (
     <div className='intro'>
 <div className="intro-left">
 <div className="i-name">
-     <span>Hey!</span>
-     <span>Rabiul</span>
+     <span>Hey guys!</span>
+     <span>Rabiul  Hossen</span>
      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus saepe, aperiam velit nisi eius distinctio dolor dicta itaque temporibus illum voluptas perspiciatis sunt quam natus autem, tenetur beatae dolore. Veniam?</span>
 </div>
-<button className='button'>Hire me</button>
-
+<button className='button i-button'>Hire me</button>
+<div className="social-icons">
+   <a href="">  <img src={facebook} style={{width:'75px'}} alt="" /></a>
+    <a href=""> <img src={github} alt="" /></a>
+    <a href=""> <img src={insta} alt="" /></a>
+</div>
 
 
 </div>
 <div className="intro-right"></div>
 
-
-
+<img src={bg} alt="" />
 
 
     </div>
